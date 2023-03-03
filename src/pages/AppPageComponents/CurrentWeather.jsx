8046e5911f71f5308ba8038f5e044eccd4c242d0
JAlbertMoody <85460733+JAlbertMoody@users.noreply.github.com>
2023-03-03 09:08:32 -0900

    Remove local storage rate limit

--- src/pages/AppPageComponents/CurrentWeather.jsx
+++ src/pages/AppPageComponents/CurrentWeather.jsx
@@ -1,28 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-function checkAPILimit() {
-
-  let apiUsage = JSON.parse(localStorage.getItem('apiUsage')) || { count: 0, timestamp: 0 };
-  
-  let now = Date.now();
-  let hourInMillis = 60 * 60 * 1000;
-  if (now - apiUsage.timestamp > hourInMillis) {
-    apiUsage.count = 0;
-    apiUsage.timestamp = now;
-  }
-  
-  if (apiUsage.count >= 250) {
-    alert('Sorry, you have exceeded the API limit for this hour. Please try again later.');
-    return false;
-  }
-  
-  apiUsage.count++;
-  localStorage.setItem('apiUsage', JSON.stringify(apiUsage));
-  
-  return true;
-}
-
-checkAPILimit()
 
 function CurrentWeather({ coordinates }) {
   const [weatherData, setWeatherData] = useState(null);
@@ -47,7 +24,6 @@ function CurrentWeather({ coordinates }) {
         }
       }
     fetchWeatherData();
-    checkAPILimit();
   
 
   }, [coordinates] );
